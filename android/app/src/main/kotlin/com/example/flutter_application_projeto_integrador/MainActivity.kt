f07990a5fa5462467d1d1257f36a9205846626c1
EduardoRPortela <142667090+EduardoRPortela@users.noreply.github.com>
2025-10-23 15:53:52 -0300

    Update commit

--- android/app/src/main/kotlin/com/example/flutter_application_projeto_integrador/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_application_projeto_integrador/MainActivity.kt
@@ -2,4 +2,4 @@ package com.example.flutter_application_projeto_integrador
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
